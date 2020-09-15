6b5c0debe71d8174757187eb9f84ff0557172035
franco <franco.labra@usach.cl>
2020-09-15 17:20:08 -0300

    arblolito

--- lab-5-script.R
+++ lab-5-script.R
@@ -16,3 +16,22 @@ colnames(cars) <- c("buyingPrice",
                     "sizeOfLuggageBoot",
                     "safety",
                     "decision")
+#cars$decision = factor(cars$decision, labels = c("unacc", "acc", "good", "vgood"))
+###################################################################
+
+#se crea el conjunto de datos para entrenamiento
+training.index = createDataPartition(cars$decision, p = 0.7)$Resample1
+training.set = cars[training.index, ]
+
+#el resto de datos son de testing
+test.set = cars[-training.index, ]
+
+#creacion del arbol
+tree = C5.0(decision ~ ., training.set)
+tree.rules = C5.0(x = training.set[, -8], y = training.set$decision, rules = T)
+tree.pred.class = predict(tree, test.set[,-8], type = "class")
+tree.pred.prob = predict(tree, test.set[,-8], type = "prob")
+
+
+conf.matrix.tree = confusionMatrix(table(test.set$decision, tree.pred.class))
+print(conf.matrix.tree)
